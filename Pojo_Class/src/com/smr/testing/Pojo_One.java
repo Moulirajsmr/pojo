73e86a94b538c011a67b592b6b746e077a579d16
Moulirajsmr <mouli.rock07@gmail.com>
2023-03-02 00:09:51 +0530

    pojo

---
+++ Pojo_Class/src/com/smr/testing/Pojo_One.java
@@ -0,0 +1,56 @@
+package com.smr.testing;
+
+public class Pojo_One {
+
+	private int a = 20;
+
+	private int b = 40;
+
+	private int c = 60;
+
+	private int d = 80;
+
+	private int e = 90;
+
+	private int f = 100;
+	
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public int getB() {
+		return b;
+	}
+	public void setB(int b) {
+		this.b = b;
+	}
+	public int getC() {
+		return c;
+	}
+	public void setC(int c) {
+		this.c = c;
+	}
+	public int getD() {
+		return d;
+	}
+	public void setD(int d) {
+		this.d = d;
+	}
+	public int getE() {
+		return e;
+	}
+	public void setE(int e) {
+		this.e = e;
+	}
+	public int getF() {
+		return f;
+	} 
+	public void setF(int f) {
+		this.f = f;
+	}
+	}
+	
+
+
---
+++ Pojo_Class/src/com/smr/testing/Pojo_Two.java
@@ -0,0 +1,31 @@
+package com.smr.testing;
+
+public class Pojo_Two {
+	
+	public static void main(String[] args) {
+		Pojo_One m = new Pojo_One();
+		int a = m.getA();
+		System.out.println(a);
+		m.setA(10);
+		System.out.println(m.getA());
+		m.setB(15);
+		System.out.println(m.getB());
+		m.setC(15);
+		System.out.println(m.getC());
+		m.setC(25);
+		System.out.println(m.getD());
+		m.setD(35);
+		System.out.println(m.getE());
+		m.setE(45);
+		System.out.println(m.getF());
+		m.setF(55);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
